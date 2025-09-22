d52d8d0077b44679d510ae56d7b319df14bae739
Lucas Zaranza <lucaszaranza@gmail.com>
2025-09-22 13:09:26 -0300

    added env url variable

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
+  process.env.NEXT_PUBLIC_API_URL ?? "https://zazastro-api.onrender.com";
 
 export async function apiFetch(path: string, init?: RequestInit) {
   const url = `${API_BASE.replace(/\/$/, "")}/${path.replace(/^\//, "")}`;
